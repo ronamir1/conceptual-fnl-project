2f5391e2a5d5cdf7540edbdd3cb6a0f3cd68bdba
ronamir <ron.amir1@mail.huji.ac.il>
2021-06-11 16:47:23 +0300

    working localserver upload

--- client/FaceDetection2.py
+++ client/FaceDetection2.py
@@ -1,7 +1,7 @@
 import time
 
 import cv2
-from config import *
+from client.config import *
 from datetime import datetime
 import uuid
 
@@ -28,16 +28,16 @@ def detect():
         faces = face_cascade.detectMultiScale(gray, 1.1, 4)
         # Draw the rectangle around each face
         for i, (x, y, w, h) in enumerate(faces):
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            roi = img[y:y + h, x:x + w]
+            # cv2.rectangle(img, (x - 0.5 * w, y - 0.5 * h), (x + 1.5 * w, y + 1.5 * h), (255, 0, 0), 2)
+            roi = img[y - int(0.5 * h) : y + int(1.5 * h), x - int(0.5 * w) :x + int(1.5 * w)]
             # Display
             uid = uuid.uuid4()
-            path_local = f"captured2\\person_{i}.jpg"
-            dest_path = f'{path_on_cloud}/entranceCam/{uid}_{datetime.now().isoformat()}.jpg'
+            path_local = f"person_{i}.jpg"
+            dest_path = f'{path_on_cloud}/entranceCam/{uid}_{datetime.now().isoformat().replace(":", "-")}.jpg'
             cv2.imwrite(path_local, roi)
             # upload to server
             storage.child(dest_path).put(path_local)
-        cv2.imshow('img', img)
+        # cv2.imshow('img', img)
         # Stop if escape key is pressed
         k = cv2.waitKey(30) & 0xff
         if k == 27:
--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import os
 import pyrebase
 import face_recognition
+from pathlib import Path
 
 app = Flask(__name__)
 
@@ -14,16 +15,69 @@ config = {"apiKey": "AIzaSyCM1MwSxh3NGOJ5MuumUcRmlZKs_b5Sp0g",
           "appId": "1:1093381045628:web:061bf8752b2bff363538f9",
           "serviceAccount": "inspectorapp-6d1d7-firebase-adminsdk-ao0uh-5ef626748a.json",
           "measurementId": "G-33MMBH01LN"}
+firebase = pyrebase.initialize_app(config)
+storage = firebase.storage()
+ENTRANCE = 'entranceCam'
+PAID = 'paidCam'
 
 
-# path_on_cloud = "bus_xxx/entranceCam/"
-# files = storage.bucket.list_blobs(path_on_cloud)
-# for f in files:
-#     print(f.name)
-#     firebase.storage().child(f.name).download("cur_im.jpg")
+def _compare_face(known_images, image_to_check):
+    result = face_recognition.compare_faces(known_images, image_to_check)
+    print(result)
+    return result
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    image_name = request.form.get('image_url')
+    bus_id = request.form.get('bus_id')
+    cam_type = request.form.get('cam_type')
+    image_url = f'{bus_id}/{cam_type}/{image_name}'
+    Path(image_url).parent.mkdir(parents=True, exist_ok=True)
+    storage.child(image_url).download(image_url)
+    new_image = face_recognition.load_image_file(image_url)
+    new_image_encodings = face_recognition.face_encodings(new_image)
+
+    if not new_image_encodings:
+        storage.delete(image_url)
+        return {'Result': 'Unrecognized image'}, 200
+    path_on_cloud = f'{bus_id}/{ENTRANCE}'
+    known_images = []
+    urls = []
+    for f in storage.bucket.list_blobs(prefix=path_on_cloud):
+        if not f.name.endswith('.jpg') or f.name == image_name:
+            continue
+        Path(f.name).parent.mkdir(parents=True, exist_ok=True)
+        storage.child(f.name).download(f.name)
+        old_image = face_recognition.load_image_file(f.name)
+        old_encodings = face_recognition.face_encodings(old_image)
+        if not old_encodings:
+            continue
+        urls.append(f.name)
+        known_images.extend(old_encodings)
+        # res = _compare_face(new_image, old_image)
+        # if res:
+        #     if cam_type == PAID:
+        #         storage.delete(f.name)
+        #         paid = True
+        #         continue
+        #     storage.delete(image_url)
+        #     return {"Result": 'Known image'}, 200
+    result = _compare_face(known_images, new_image_encodings[0])
+    response = {'Result': 'Unknown image'}, 200
+    for url, res in zip(urls, result):
+        if res:
+            storage.delete(url)
+            if cam_type == PAID:
+                response = {'Result': 'User paid'}, 200
+
+    if cam_type == PAID:
+        storage.delete(image_url)
+    return response
+
 
 @app.route("/compare", methods=["POST"])
-def compare_faces(request):
+def compare_faces():
     """HTTP Cloud Function.
     Args:
         request (flask.Request): The request object.
@@ -33,33 +87,37 @@ def compare_faces(request):
         Response object using `make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
-    origin = request.args.get("origin")
-    dest = request.args.get("dest")
-    firebase = pyrebase.initialize_app(config)
-    storage = firebase.storage()
-
+    origin = request.form.get("origin")
+    dest = request.form.get("dest")
     for f in storage.bucket.list_blobs(prefix=origin):
-        if not f.name.endwith('.jpg'):
-            break
-        firebase.storage().child(f.name).download(f.name)
+        if not f.name.endswith('.jpg'):
+            continue
+        fname = f.name.replace(':', '-')
+        Path(fname).parent.mkdir(parents=True, exist_ok=True)
+        storage.child(f.name).download(fname)
     for f in storage.bucket.list_blobs(prefix=dest):
-        if not f.name.endwith('.jpg'):
-            break
-        firebase.storage().child(f.name).download(f.name)
+        if not f.name.endswith('.jpg'):
+            continue
+        fname = f.name.replace(':', '-')
+        Path(fname).parent.mkdir(parents=True, exist_ok=True)
+        storage.child(f.name).download(fname)
     results = {}
-    for file in os.listdir(origin):
-        if file.endswith(".jpg"):
-            known_image = face_recognition.load_image_file(origin + file)
-            for file2 in os.listdir(dest):
-                if file.endswith(".jpg"):
-                    unknown_image = face_recognition.load_image_file(dest + file2)
-                    file_encoding = face_recognition.face_encodings(known_image)[0]
+    for f1 in os.listdir(origin):
+        if f1.endswith(".jpg"):
+            known_image = face_recognition.load_image_file(os.path.join(origin, f1))
+            for f2 in os.listdir(dest):
+                if f1.endswith(".jpg"):
+                    unknown_image = face_recognition.load_image_file(os.path.join(dest, f2))
+                    file_encodings = face_recognition.face_encodings(known_image)
+                    file_encoding = file_encodings[0]
                     file2_encoding = face_recognition.face_encodings(unknown_image)
                     if len(file2_encoding) == 0:
-                        print("unknown file" + file2 + " was not recognized")
+                        print("unknown file" + f2 + " was not recognized")
                     else:
                         result = face_recognition.compare_faces([file_encoding], file2_encoding[0])
-                        results[origin + file] = result
+                        results[origin + f1] = result
+                        print(f'{f2} result is {result}')
+
     return results
 
     # known_image = face_recognition.load_image_file(f.name)
