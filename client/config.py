3f1e9525ae8466bc63e1e4152dcb2b9d09c4ddc6
ilanerukh <ilanerukh@gmail.com>
2021-06-30 09:49:29 +0300

    env config

--- client/config.py
+++ client/config.py
@@ -1,19 +1,23 @@
 import pyrebase
+import os
+from dotenv import load_dotenv
 
-config = {"apiKey": "AIzaSyCM1MwSxh3NGOJ5MuumUcRmlZKs_b5Sp0g",
-          "authDomain": "inspectorapp-6d1d7.firebaseapp.com",
-          "databaseURL": "https://inspectorapp-6d1d7.firebaseapp.com",
-          "projectId": "inspectorapp-6d1d7",
-          "storageBucket": "inspectorapp-6d1d7.appspot.com",
-          "messagingSenderId": "1093381045628",
-          "appId": "1:1093381045628:web:061bf8752b2bff363538f9",
-          "serviceAccount": "inspectorapp-6d1d7-firebase-adminsdk-ao0uh-5ef626748a.json",
-          "measurementId": "G-33MMBH01LN"}
+load_dotenv('.env')
+
+config = {"apiKey": os.environ['API_KEY'],
+          "authDomain": os.environ['AUTH_DOMAIN'],
+          "databaseURL": os.environ['DATABASE_URL'],
+          "projectId": os.environ['PROJECT_ID'],
+          "storageBucket": os.environ['STORAGE_BUCKET'],
+          "messagingSenderId": os.environ['MESSAGING_SENDER_ID'],
+          "appId": os.environ['APP_ID'],
+          "serviceAccount": os.environ['SERVICE_ACOUNT'],
+          "measurementId": os.environ['MEASURMENT_ID']}
 
 firebase = pyrebase.initialize_app(config)
 storage = firebase.storage()
-bus_id = 1234
-index = 1
-cam_type = 'paidCam'
-xml_path = r"../xmls/haarcascade_frontalface_default.xml"
-BASE_API = r'https://inspector-service-354e3fpima-ew.a.run.app'
+bus_id = os.environ['BUS_ID']
+index = os.environ['INDEX']
+cam_type = os.environ['CAM_TYPE']
+xml_path = os.environ['XML_PATH']
+BASE_API = os.environ['BASE_API']
